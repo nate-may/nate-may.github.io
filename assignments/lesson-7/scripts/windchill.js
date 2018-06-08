daf4d090a7eb99a66e7f8301e86caa7d58d5e9eb
natetheskate1990 <35317992+natetheskate1990@users.noreply.github.com>
2018-06-08 11:07:03 -0500

    fixed windchill, started gallery page

---
+++ assignments/lesson-7/scripts/windchill.js
@@ -0,0 +1,14 @@
+//f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16
+
+//var f = windChill;
+
+
+var s = 5;
+var h = 90;
+var l = 60;
+var a = (h + l) / 2;
+windChill = 35.74 + .6215 * a - 35.75 * Math.pow(s, 0.16) + 0.4275 * a * Math.pow(s, 0.16);
+
+
+document.getElementById("windChill").innerHTML = Math.round(windChill);
+//document.getElementById("currentdate").innerHTML = n + ", " + d.getDate() + " " + m + " " + d.getFullYear();
