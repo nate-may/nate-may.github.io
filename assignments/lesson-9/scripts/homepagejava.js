7a08702d1f97c9ea4dcfdc227b4d6d554ef94f7b
natetheskate1990 <35317992+natetheskate1990@users.noreply.github.com>
2018-06-26 19:19:01 -0500

    fixing Franklin-9 homepage

--- assignments/lesson-9/scripts/homepagejava.js
+++ assignments/lesson-9/scripts/homepagejava.js
@@ -1,14 +1,14 @@
-var header = document.querySelector('header');
+/*var header = document.querySelector('header');*/
 var section = document.querySelector('section');
 var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
-request.onload = function() {
-    var superHeroes = request.response;
-    populateHeader(---);
-    showHeroes(---);
+request.onload = function () {
+    var townInfo = request.response;
+    showData(townInfo);
+    showHeroes(-- - );
 }
 //JSON will go in here, values, statistics, and display on HTML
 
@@ -22,36 +22,42 @@ request.onload = function() {
     header.appendChild(myPara);
 }*/
 
-function showHeroes(jsonObj) {
-    var heroes = jsonObj['members'];
+function showData(jsonObj) {
+    var cities = jsonObj['towns'];
 
-    for (var i = 0; i < heroes.length; i++) {
+    for (var i = 0; i < cities.length; i++) {
+        if (i == 2) continue;
+
         var myArticle = document.createElement('article');
         var myH2 = document.createElement('h2');
+        var myH3 = document.createElement('h3');
         var myPara1 = document.createElement('p');
         var myPara2 = document.createElement('p');
         var myPara3 = document.createElement('p');
+        var myPara4 = document.createElement('p');
         var myList = document.createElement('ul');
 
-        myH2.textContent = heroes[i].name;
-        myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-        myPara2.textContent = 'Age: ' + heroes[i].age;
-        myPara3.textContent = 'Superpowers:';
+        myH2.textContent = cities[i].name;
+        myH3.textContent = 'Motto: ' + cities[i].motto;
+        myPara1.textContent = 'Year Founded: ' + cities[i].yearFounded;
+        myPara2.textContent = 'Population: ' + cities[i].currentPopulation;
+        myPara3.textContent = 'Average Rainfall:' + cities[i].averageRainfall;
+        myPara4.textContent = 'Upcoming Events:';
 
-        /*var superPowers = heroes[i].powers;
-        for (var j = 0; j < superPowers.length; j++) {
+        var cityEvents = cities[i].events;
+        for (var j = 0; j < cityEvents.length; j++) {
             var listItem = document.createElement('li');
-            listItem.textContent = superPowers[j];
+            listItem.textContent = cityEvents[j];
             myList.appendChild(listItem);
         }
-*/
-      /*  myArticle.appendChild(myH2);
+
+        myArticle.appendChild(myH2);
         myArticle.appendChild(myPara1);
         myArticle.appendChild(myPara2);
         myArticle.appendChild(myPara3);
         myArticle.appendChild(myList);
 
-        section.appendChild(myArticle);*/
+        section.appendChild(myArticle);
     }
 }
 /*
